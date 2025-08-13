39a9ffc8f1c36ab4a288151e46c7a709747561ac
Benitosalain <163571668+Benitosalain@users.noreply.github.com>
2025-08-13 14:48:40 +0200

    Update Scanner.jsx

--- src/Scanner.jsx
+++ src/Scanner.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { TrendingUp } from 'lucide-react';
-import { canonicalizeSymbol, extractPairFromOpportunity } from "./utils/symbol-mapping";
+import { canonicalizeSymbol, extractPairFromOpportunity } from "./symbol-mapping.js";
 
 const API_BASE = process.env.REACT_APP_API_BASE_URL;
 
