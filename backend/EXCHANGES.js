e6116101fa8c7de76058d336f60bbb754be28b7b
Benitosalain <163571668+Benitosalain@users.noreply.github.com>
2025-09-21 10:47:22 +0200

    Update EXCHANGES.js
    updates on exchanges

--- backend/EXCHANGES.js
+++ backend/EXCHANGES.js
@@ -6,13 +6,15 @@ export const EXCHANGES = [
     api: 'https://api.binance.com/api/v3/ticker/price?symbol=',
     symbols: [
       'BTC-USDT', 'ETH-USDT', 'LTC-USDT', 'BCH-USDT', 'BNB-USDT', 'SOL-USDT', 'ADA-USDT', 'XRP-USDT',
-      'DOT-USDT', 'DOGE-USDT', 'AVAX-USDT', 'POL-USDT', // <-- migrated from MATIC-USDT
+      'DOT-USDT', 'DOGE-USDT', 'AVAX-USDT', 'POL-USDT',
       'SHIB-USDT', 'TRX-USDT', 'LINK-USDT', 'ATOM-USDT',
       'XMR-USDT', 'WBTC-USDT', 'UNI-USDT', 'XTZ-USDT', 'FIL-USDT', 'EOS-USDT', 'AAVE-USDT', 'SAND-USDT',
       'APE-USDT', 'GRT-USDT', 'ETC-USDT', 'NEAR-USDT', 'EGLD-USDT', 'ZEC-USDT', 'STX-USDT', 'ALGO-USDT',
       'SNX-USDT', 'RUNE-USDT', 'MKR-USDT', 'CRV-USDT', 'DYDX-USDT', 'LDO-USDT', '1INCH-USDT', 'COMP-USDT',
+      'ARB-USDT', 'TON-USDT', 'OP-USDT', 'USDC-USDT', 'DAI-USDT',
+      'BTC-USD', 'ETH-USD', 'LTC-USD', 'SOL-USD',
       // Cross-asset for triangles:
-      'ETH-BTC', 'LTC-BTC', 'XRP-BTC', 'BNB-BTC', 'SOL-BTC', 'ADA-BTC'
+      'ETH-BTC', 'LTC-BTC', 'XRP-BTC', 'BNB-BTC', 'SOL-BTC', 'ADA-BTC', 'ARB-BTC', 'TON-BTC', 'OP-BTC'
     ]
   },
   {
@@ -21,7 +23,12 @@ export const EXCHANGES = [
     symbols: [
       'BTC-USD', 'ETH-USD', 'LTC-USD', 'XRP-USD', 'BCH-USD', 'SOL-USD', 'ADA-USD', 'DOT-USD',
       'DOGE-USD', 'AVAX-USD', 'MATIC-USD', 'TRX-USD', 'LINK-USD', 'ATOM-USD', 'UNI-USD', 'XTZ-USD',
-      'FIL-USD', 'EOS-USD', 'AAVE-USD', 'ETC-USD', 'NEAR-USD', 'ALGO-USD', 'ETH-BTC'
+      'FIL-USD', 'EOS-USD', 'AAVE-USD', 'ETC-USD', 'NEAR-USD', 'ALGO-USD', 'ETH-BTC',
+      // Additional pairs from symbols.json
+      'COMP-USDT', 'MKR-USDT', 'ZEC-USDT',
+      'BTC-EUR', 'ETH-EUR', 'LTC-EUR',
+      'BTC-GBP', 'ETH-GBP', 'LTC-GBP',
+      'BTC-JPY', 'ETH-JPY', 'LTC-JPY',
     ]
   },
   {
@@ -30,7 +37,8 @@ export const EXCHANGES = [
     symbols: [
       'BTC-USD', 'ETH-USD', 'LTC-USD', 'XRP-USD', 'BCH-USD', 'SOL-USD', 'ADA-USD', 'DOT-USD',
       'DOGE-USD', 'AVAX-USD', 'MATIC-USD', 'TRX-USD', 'LINK-USD', 'ATOM-USD', 'UNI-USD', 'XTZ-USD',
-      'FIL-USD', 'EOS-USD', 'AAVE-USD', 'ETC-USD', 'NEAR-USD', 'ALGO-USD', 'ETH-BTC'
+      'FIL-USD', 'EOS-USD', 'AAVE-USD', 'ETC-USD', 'NEAR-USD', 'ALGO-USD', 'ETH-BTC',
+      'COMP-USDT', 'MKR-USDT',
     ]
   },
   {
@@ -38,9 +46,10 @@ export const EXCHANGES = [
     api: 'https://api.huobi.pro/market/detail/merged?symbol=',
     symbols: [
       'BTC-USDT', 'ETH-USDT', 'LTC-USDT', 'BCH-USDT', 'BNB-USDT', 'SOL-USDT', 'ADA-USDT', 'XRP-USDT',
-      'DOT-USDT', 'DOGE-USDT', 'AVAX-USDT', 'MATIC-USDT', 'TRX-USDT', 'LINK-USDT', 'ATOM-USDT',
+      'DOT-USDT', 'DOGE-USDT', 'AVAX-USDT', 'MATIC-USDT', 'SHIB-USDT', 'TRX-USDT', 'LINK-USDT', 'ATOM-USDT',
       'UNI-USDT', 'XTZ-USDT', 'FIL-USDT', 'EOS-USDT', 'AAVE-USDT', 'ETC-USDT', 'NEAR-USDT', 'ALGO-USDT',
-      'ETH-BTC'
+      'ETH-BTC', 'LTC-BTC', 'XRP-BTC', 'SOL-BTC', 'ADA-BTC',
+      'COMP-USDT', 'MKR-USDT',
     ]
   },
   {
@@ -48,9 +57,20 @@ export const EXCHANGES = [
     api: 'https://www.okex.com/api/spot/v3/instruments/',
     symbols: [
       'BTC-USDT', 'ETH-USDT', 'LTC-USDT', 'BCH-USDT', 'BNB-USDT', 'SOL-USDT', 'ADA-USDT', 'XRP-USDT',
-      'DOT-USDT', 'DOGE-USDT', 'AVAX-USDT', 'MATIC-USDT', 'TRX-USDT', 'LINK-USDT', 'ATOM-USDT',
-      'UNI-USDT', 'XTZ-USDT', 'FIL-USDT', 'EOS-USDT', 'AAVE-USDT', 'ETC-USDT', 'NEAR-USDT', 'ALGO-USDT',
-      'ETH-BTC'
+      'DOT-USDT', 'DOGE-USDT', 'AVAX-USDT', 'MATIC-USDT', 'SHIB-USDT', 'TRX-USDT', 'LINK-USDT', 'ATOM-USDT',
+      'UNI-USDT', 'XTZ-USDT', 'FIL-USDT', 'EOS-USDT', 'AAVE-USDT', 'SAND-USDT', 'APE-USDT', 'GRT-USDT',
+      'ETC-USDT', 'NEAR-USDT', 'EGLD-USDT', 'ZEC-USDT', 'STX-USDT', 'ALGO-USDT', 'SNX-USDT', 'RUNE-USDT',
+      'MKR-USDT', 'CRV-USDT', 'DYDX-USDT', 'LDO-USDT', '1INCH-USDT', 'COMP-USDT', 'ETH-BTC',
+      'ARB-USDT', 'ARB-BTC', 'OP-USDT', 'OP-BTC', 'TON-USDT', 'TON-BTC',
+      'OKB-USDT', 'OKB-BTC', 'OKB-ETH',
+      'KCS-USDT', 'KCS-BTC', 'KCS-ETH',
+      'HT-USDT', 'HT-BTC', 'HT-ETH',
+      'DASH-USDT', 'DASH-BTC', 'DASH-ETH',
+      'MANA-USDT', 'MANA-BTC', 'MANA-ETH',
+      'AXS-USDT', 'AXS-BTC', 'AXS-ETH',
+      'GALA-USDT', 'GALA-BTC', 'GALA-ETH',
+      'AR-USDT', 'AR-BTC', 'AR-ETH',
+      'LTC-BTC', 'XRP-BTC', 'BNB-BTC', 'SOL-BTC', 'ADA-BTC', 'WBTC-USDT'
     ]
   },
   {
@@ -58,9 +78,10 @@ export const EXCHANGES = [
     api: 'https://api.gateio.ws/api/v4/spot/tickers?currency_pair=',
     symbols: [
       'BTC-USDT', 'ETH-USDT', 'LTC-USDT', 'BCH-USDT', 'BNB-USDT', 'SOL-USDT', 'ADA-USDT', 'XRP-USDT',
-      'DOT-USDT', 'DOGE-USDT', 'AVAX-USDT', 'MATIC-USDT', 'TRX-USDT', 'LINK-USDT', 'ATOM-USDT',
-      'UNI-USDT', 'XTZ-USDT', 'FIL-USDT', 'EOS-USDT', 'AAVE-USDT', 'ETC-USDT', 'NEAR-USDT', 'ALGO-USDT',
-      'ETH-BTC'
+      'DOT-USDT', 'DOGE-USDT', 'AVAX-USDT', 'MATIC-USDT', 'SHIB-USDT', 'TRX-USDT', 'LINK-USDT', 'ATOM-USDT',
+      'UNI-USDT', 'XTZ-USDT', 'FIL-USDT', 'EOS-USDT', 'AAVE-USDT', 'SAND-USDT', 'APE-USDT', 'GRT-USDT',
+      'ETC-USDT', 'NEAR-USDT', 'EGLD-USDT', 'ZEC-USDT', 'STX-USDT', 'ALGO-USDT', 'SNX-USDT', 'RUNE-USDT',
+      'MKR-USDT', 'CRV-USDT', 'DYDX-USDT', 'LDO-USDT', '1INCH-USDT', 'COMP-USDT', 'ETH-BTC'
     ]
   },
   {
@@ -68,9 +89,12 @@ export const EXCHANGES = [
     api: 'https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=',
     symbols: [
       'BTC-USDT', 'ETH-USDT', 'LTC-USDT', 'BCH-USDT', 'BNB-USDT', 'SOL-USDT', 'ADA-USDT', 'XRP-USDT',
-      'DOT-USDT', 'DOGE-USDT', 'AVAX-USDT', 'MATIC-USDT', 'TRX-USDT', 'LINK-USDT', 'ATOM-USDT',
-      'UNI-USDT', 'XTZ-USDT', 'FIL-USDT', 'EOS-USDT', 'AAVE-USDT', 'ETC-USDT', 'NEAR-USDT', 'ALGO-USDT',
-      'ETH-BTC'
+      'DOT-USDT', 'DOGE-USDT', 'AVAX-USDT', 'MATIC-USDT', 'SHIB-USDT', 'TRX-USDT', 'LINK-USDT', 'ATOM-USDT',
+      'UNI-USDT', 'XTZ-USDT', 'FIL-USDT', 'EOS-USDT', 'AAVE-USDT', 'SAND-USDT', 'APE-USDT', 'GRT-USDT',
+      'ETC-USDT', 'NEAR-USDT', 'EGLD-USDT', 'ZEC-USDT', 'STX-USDT', 'ALGO-USDT', 'SNX-USDT', 'RUNE-USDT',
+      'MKR-USDT', 'CRV-USDT', 'DYDX-USDT', 'LDO-USDT', '1INCH-USDT', 'COMP-USDT', 'ETH-BTC',
+      'ARB-USDT', 'ARB-BTC', 'TON-USDT', 'TON-BTC', 'OP-USDT', 'OP-BTC',
+      'KCS-USDT', 'KCS-BTC', 'KCS-ETH',
     ]
   },
   {
@@ -79,7 +103,8 @@ export const EXCHANGES = [
     symbols: [
       'BTC-USD', 'ETH-USD', 'LTC-USD', 'XRP-USD', 'BCH-USD', 'SOL-USD', 'ADA-USD', 'DOT-USD',
       'DOGE-USD', 'AVAX-USD', 'MATIC-USD', 'TRX-USD', 'LINK-USD', 'UNI-USD', 'XTZ-USD', 'FIL-USD',
-      'EOS-USD', 'AAVE-USD', 'ETC-USD', 'ALGO-USD', 'ETH-BTC'
+      'EOS-USD', 'AAVE-USD', 'ETC-USD', 'ALGO-USD', 'ETH-BTC',
+      'BTC-EUR', 'ETH-EUR', 'LTC-EUR',
     ]
   },
   {
@@ -88,7 +113,7 @@ export const EXCHANGES = [
     symbols: [
       'BTC-USD', 'ETH-USD', 'LTC-USD', 'BCH-USD', 'SOL-USD', 'ADA-USD', 'XRP-USD', 'DOT-USD',
       'DOGE-USD', 'MATIC-USD', 'LINK-USD', 'UNI-USD', 'XTZ-USD', 'FIL-USD', 'EOS-USD', 'AAVE-USD',
-      'ETC-USD', 'ALGO-USD', 'ETH-BTC'
+      'ETC-USD', 'ALGO-USD', 'ETH-BTC',
     ]
   },
   {
@@ -108,7 +133,7 @@ export const EXCHANGES = [
       'BTC-USDT', 'ETH-USDT', 'LTC-USDT', 'BCH-USDT', 'BNB-USDT', 'SOL-USDT', 'ADA-USDT', 'XRP-USDT',
       'DOT-USDT', 'DOGE-USDT', 'AVAX-USDT', 'MATIC-USDT', 'TRX-USDT', 'LINK-USDT', 'ATOM-USDT',
       'UNI-USDT', 'XTZ-USDT', 'FIL-USDT', 'EOS-USDT', 'AAVE-USDT', 'ETC-USDT', 'NEAR-USDT', 'ALGO-USDT',
-      'ETH-BTC'
+      'ETH-BTC',
     ]
   },
   // For all other exchanges, use dynamic expansion:
@@ -133,34 +158,39 @@ export const EXCHANGES = [
     symbols: Object.keys(pricesByExchange['Bybit'] || {})
   },
   {
-    name: 'CoinbasePro',
-    api: 'https://api.exchange.coinbase.com/products',
-    symbols: Object.keys(pricesByExchange['CoinbasePro'] || {})
+    name: 'Coinbase',
+    api: 'https://api.coinbase.com/v2/prices',
+    symbols: Object.keys(pricesByExchange['Coinbase'] || {})
   },
   {
-    name: 'CryptoCom',
-    api: 'https://api.crypto.com/v2/public/get-ticker',
-    symbols: Object.keys(pricesByExchange['CryptoCom'] || {})
+    name: 'Bitrue',
+    api: 'https://www.bitrue.com/api/v1/ticker/price?symbol=',
+    symbols: Object.keys(pricesByExchange['Bitrue'] || {})
   },
   {
-    name: 'LBank',
-    api: 'https://api.lbank.info/v2/ticker.do?symbol=all',
-    symbols: Object.keys(pricesByExchange['LBank'] || {})
+    name: 'BithumbGlobal',
+    api: 'https://global-openapi.bithumb.pro/openapi/v1/spot/ticker?symbol=',
+    symbols: Object.keys(pricesByExchange['BithumbGlobal'] || {})
   },
   {
-    name: 'MEXC',
-    api: 'https://api.mexc.com/api/v3/ticker/price',
-    symbols: Object.keys(pricesByExchange['MEXC'] || {})
+    name: 'Bitmart',
+    api: 'https://api.bitmart.com/v2/ticker?symbol=',
+    symbols: Object.keys(pricesByExchange['Bitmart'] || {})
   },
   {
-    name: 'OneInch',
-    api: 'https://api.1inch.dev/swap/v5.2/1/quote',
-    symbols: Object.keys(pricesByExchange['OneInch'] || {})
+    name: 'XTcom',
+    api: 'https://sapi.xt.com/v4/public/ticker/price?symbol=',
+    symbols: Object.keys(pricesByExchange['XTcom'] || {})
   },
   {
-    name: 'PancakeSwap',
-    api: 'https://api.1inch.dev/swap/v5.2/56/quote',
-    symbols: Object.keys(pricesByExchange['PancakeSwap'] || {})
+    name: 'Deribit',
+    api: 'https://www.deribit.com/api/v2/public/ticker?instrument_name=',
+    symbols: Object.keys(pricesByExchange['Deribit'] || {})
+  },
+  {
+    name: 'CryptoComEnhanced',
+    api: 'https://api.crypto.com/v2/public/get-ticker?instrument_name=',
+    symbols: Object.keys(pricesByExchange['CryptoComEnhanced'] || {})
   },
   {
     name: 'Phemex',
