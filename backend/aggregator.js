8deb0fa63d26b335ba65c038675746aa908808a1
Benitosalain <163571668+Benitosalain@users.noreply.github.com>
2025-10-22 17:08:19 +0200

    Update aggregator.js

--- backend/aggregator.js
+++ backend/aggregator.js
@@ -1,7 +1,6 @@
 import { SYMBOLS } from "./symbols.js";
 import { pricesByExchange } from "./symbol-mapping.js";
 import * as binance from "./adapters/binance.js";
-import * as binanceus from "./adapters/binanceus.js";
 
 export async function getAllPrices() {
   const results = {};
@@ -17,15 +16,4 @@ export async function getAllPrices() {
       }
     }
     // BinanceUS
-    const binanceUSSymbol = pricesByExchange['BinanceUS'][symbol];
-    if (binanceUSSymbol) {
-      try {
-        results[symbol].BinanceUS = await binanceus.fetchBinanceUSTicker(binanceUSSymbol);
-      } catch (e) {
-        console.log(`BinanceUS missing ${symbol}:`, e.message);
-      }
-    }
-    // ...repeat for other exchanges...
-  }
-  return results;
-}
+   
